577464499867896f4781c11ef45ad9a47ab9c14f
lrorpilla <leorafaelorpilla@gmail.com>
2021-12-16 10:24:47 +1000

    Adapt to theme

--- lib/widgets/heading.dart
+++ lib/widgets/heading.dart
@@ -2,15 +2,6 @@ import 'package:contentful_rich_text/types/blocks.dart';
 import 'package:contentful_rich_text/types/types.dart';
 import 'package:flutter/material.dart';
 
-Map<BLOCKS, double> _defaultHeadingSizes = {
-  BLOCKS.HEADING_1: 24.0,
-  BLOCKS.HEADING_2: 22.0,
-  BLOCKS.HEADING_3: 20.0,
-  BLOCKS.HEADING_4: 18.0,
-  BLOCKS.HEADING_5: 16.0,
-  BLOCKS.HEADING_6: 14.0,
-};
-
 class Heading extends StatelessWidget {
   final BLOCKS level;
   final String text;
@@ -26,6 +17,25 @@ class Heading extends StatelessWidget {
     required this.next,
   });
 
+  TextStyle getTextStyle(BuildContext context, BLOCKS block) {
+    switch (block) {
+      case BLOCKS.HEADING_1:
+        return Theme.of(context).textTheme.heading1;
+      case BLOCKS.HEADING_2:
+        return Theme.of(context).textTheme.heading2;
+      case BLOCKS.HEADING_3:
+        return Theme.of(context).textTheme.heading3;
+      case BLOCKS.HEADING_4:
+        return Theme.of(context).textTheme.heading4;
+      case BLOCKS.HEADING_5:
+        return Theme.of(context).textTheme.heading5;
+      case BLOCKS.HEADING_6:
+        return Theme.of(context).textTheme.heading6;
+      default:
+        throw Exception("Unimplemented block type");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text.rich(
@@ -34,10 +44,7 @@ class Heading extends StatelessWidget {
           TextSpan(
             text: text,
             children: next(content),
-            style: TextStyle(
-              fontWeight: fontWeight,
-              fontSize: _defaultHeadingSizes[level],
-            ),
+            style: getTextStyle(context, level),
           ),
         ],
       ),
